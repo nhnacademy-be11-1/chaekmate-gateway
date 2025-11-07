34d478da974320145d96b4d47a24a0cc18ac197d
chzhxkd100 <chzhxkd100@naver.com>
2025-11-07 16:47:13 +0900

    fix: CI-cd 포트넘버이름수정

--- src/test/java/shop/chaekmate/gateway/GatewayApplicationTests.java
+++ src/test/java/shop/chaekmate/gateway/GatewayApplicationTests.java
@@ -2,10 +2,8 @@ package shop.chaekmate.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class GatewayApplicationTests {
 
 	@Test
