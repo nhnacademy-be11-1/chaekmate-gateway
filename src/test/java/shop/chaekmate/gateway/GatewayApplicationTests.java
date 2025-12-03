f62ae66d7db06e17f84b5da3fd9027a231fb9f2b
chzhxkd100 <chzhxkd100@naver.com>
2025-12-03 16:31:53 +0900

    feat: 로그 행동 분석 OTEL 에서 micrometer 로 변경

--- src/test/java/shop/chaekmate/gateway/GatewayApplicationTests.java
+++ src/test/java/shop/chaekmate/gateway/GatewayApplicationTests.java
@@ -1,24 +1,12 @@
 package shop.chaekmate.gateway;
 
-import io.opentelemetry.sdk.OpenTelemetrySdk;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class GatewayApplicationTests {
-
-    @Configuration
-    static class TestConfig {
-        @Bean
-        public OpenTelemetrySdk openTelemetrySdk() {
-            return Mockito.mock(OpenTelemetrySdk.class); // OTEL SDK Mock
-        }
-    }
 
 	@Test
 	void contextLoads() {
